4298e4ab6a481dca3cac886a85eac343cd02059a
Dimo4ka <dmitry.suhotsky@gmail.com>
2024-11-08 12:58:36 +0400

    added sorting for games

--- src/pages/games/index.tsx
+++ src/pages/games/index.tsx
@@ -9,6 +9,15 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 const Games = () => {
   const { games, loading, deleteGame } = useGames()
+    // const sortedItems = games.sort((a, b) => {
+    //     return Date.parse(a.date) - Date.parse(b.date);
+    // });
+    const sortedItems = games.sort((b, a) => {
+        const [hoursA, minutesA] = a.date.split(':').map(Number);
+        const [hoursB, minutesB] = b.date.split(':').map(Number);
+
+        return hoursA - hoursB || minutesA - minutesB;
+    });
   return (
     <>
       <h1>Игры вечера</h1>
@@ -16,11 +25,10 @@ const Games = () => {
             games.length === 0 ? <div>Нет начавшихся игр</div> : null
         }
         {
-            loading ? <CircularProgress /> :
-          games.map((item: IInfoGame) => {
+            loading ? <CircularProgress /> : sortedItems.map((item: IInfoGame) => {
             return (
               <div key={item.id} className="game">
-                <Link to={`${item.id}`}>
+                <Link to={`${item.id}`} style={{textDecoration: 'none'}}>
                   <div><Typography variant='button'>Игра № {item.numberGame} </Typography></div>
                   <Typography variant='body2'>{(item.date)} </Typography>
 
